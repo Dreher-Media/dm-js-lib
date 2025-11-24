d0dcdad9ef12aca50de292b2a835968946107ed7
Tobias Laufersweiler <me@tobiascodes.de>
2025-11-24 18:30:06 +0100

    add version number to build

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,12 +1,17 @@
 import terser from "@rollup/plugin-terser";
 import typescript from "@rollup/plugin-typescript";
+import { readFileSync } from "fs";
+
+const packageJson = JSON.parse(readFileSync("./package.json", "utf-8"));
+const version = packageJson.version;
 
 export default {
   input: "src/index.ts",
   output: {
     file: "dist/dm-js-lib.min.js",
     format: "iife",
-    name: "DmJsLib"
+    name: "DmJsLib",
+    banner: `/*! dm-js-lib v${version} */`
   },
   plugins: [
     typescript({
